dafe2946644bdfd95a8f00c6c8e429e6bae54418
viktormelker <viktor.melker@fyndiq.com>
2017-10-11 12:20:11 +0200

    Added decorators stolen copied from order-api and order-read

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# new-platform-service-template
+# global-platform-common
+This repository contains functionalities that are generic and can be used in more than
+one service.
 
---
+++ src/decorators.py
@@ -0,0 +1,47 @@
+from functools import wraps
+from typing import Callable
+
+import cerberus
+from sanic import response
+
+from app import logger
+
+
+class ValidateRequest:
+    def __init__(self, validator_schema: dict) -> None:
+        self.validator_schema = validator_schema
+
+    def __call__(self, view_function: Callable) -> None:
+
+        @wraps(view_function)
+        def wrapped_view_function(*args, **kwargs):
+            request = args[0]
+            payload = request.json or {}
+
+            validator = cerberus.Validator(self.validator_schema)
+            if not validator.validate(payload):
+                logger.info(
+                    "Received data is not valid!",
+                    payload=payload, errors=validator.errors
+                )
+                return response.json(validator.errors, 400)
+
+            return view_function(request, **kwargs)
+
+        return wrapped_view_function
+
+
+def in_state(allowed_states):
+    def wrapper(func):
+        @wraps(func)
+        def _in_state(*args, **kwargs):
+            instance, _ = args
+            if instance.state in allowed_states:
+                return func(*args, **kwargs)
+            logger.warning(
+                "Invalid aggregate state", method=func.__name__,
+                current_state=instance.state,
+                allowed_states=[e for e in allowed_states]
+            )
+        return _in_state
+    return wrapper
