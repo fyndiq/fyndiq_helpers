17ca46e590acc73b2d3f25d686a73fbe8180033a
viktormelker <viktor.melker@fyndiq.com>
2017-10-12 09:46:39 +0200

    renamed stuff

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="global_platform_common",
+    name="global_platform_helpers",
     version="0.1.0",
     description="Helpers for fyndiq services",
-    url="https://github.com/fyndiq/global_platform_common",
+    url="https://github.com/fyndiq/global_platform_helpers",
     author="Fyndiq AB",
     author_email="support@fyndiq.com",
     license="MIT",
