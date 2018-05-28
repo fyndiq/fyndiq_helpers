d410c051dc6c9ee239850b6bd93a9f2ce9bd642b
Viktor Melker <viktormelker@gmail.com>
2018-05-28 11:44:50 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fyndiq_helpers",
-    version="0.1.6",
+    version="0.1.7",
     description="Helpers for fyndiq services",
     url="https://github.com/fyndiq/fyndiq_helpers",
     author="Fyndiq AB",
