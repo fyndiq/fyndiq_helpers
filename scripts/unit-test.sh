8202689fc5871b9ccf5815a3d7d84d5c557ae41d
Viktor Melker <viktormelker@gmail.com>
2019-01-15 09:22:19 +0100

    Fix codecov stuff

--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-CONFIG=test pytest --cov=app/ --junitxml=/tmp/test-results/report.xml --no-cov-on-fail --cov-report term-missing
+CONFIG=test pytest --cov=fyndiq_helpers/ --junitxml=/tmp/test-results/report.xml --no-cov-on-fail --cov-report term-missing
 
 if [[ ! -z "${CODECOV_TOKEN}" ]]; then
     codecov -t $CODECOV_TOKEN
